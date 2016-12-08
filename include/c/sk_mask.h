8fe3b3182ed94eeff27baf2b7d59940b15fa888c
Tyler Hoeflicker <thoeflicker@bluebeam.com>
2016-12-08 09:35:33 -0800

    add missing conditional directive

--- include/c/sk_mask.h
+++ include/c/sk_mask.h
@@ -18,3 +18,5 @@ SK_C_PLUS_PLUS_BEGIN_GUARD
 SK_API sk_mask_t* sk_mask_new(uint8_t* pixels, const sk_mask_format_t format, uint32_t row_bytes, sk_irect_t* crect);
 SK_API void sk_mask_destructor(sk_mask_t* cmask);
 SK_API sk_color_t get_pixel_color(sk_mask_t* cmask, int x, int y);
+
+#endif
