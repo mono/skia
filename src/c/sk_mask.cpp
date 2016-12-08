ae338127d79b8e40f02b9a7ad167c9beaa45e10b
Tyler Hoeflicker <thoeflicker@bluebeam.com>
2016-12-08 10:55:28 -0800

    fix build errors

--- src/c/sk_mask.cpp
+++ src/c/sk_mask.cpp
@@ -8,15 +8,17 @@
 #include "SkMask.h"
 #include "SkColor.h"
 #include "SkColorPriv.h"
-#include "sk_types.h"
+
 #include "sk_mask.h"
+#include "sk_types_priv.h"
+#include "sk_types.h"
 
 sk_mask_t* sk_mask_new(uint8_t* pixels, const sk_mask_format_t format, uint32_t row_bytes, sk_irect_t* crect) {
     SkMask* mask = new SkMask();
     mask->fImage = pixels;
     mask->fFormat = (SkMask::Format)format;
     mask->fRowBytes = row_bytes;
-    mask-fBounds = *AsIRect(crect);
+    mask->fBounds = *AsIRect(crect);
     return ToMask(mask);
 }
 
@@ -29,13 +31,13 @@ sk_color_t get_pixel_color(sk_mask_t* cmask, int x, int y) {
     switch (mask->fFormat) {
         default:
         case SkMask::Format::kBW_Format:
-            return SkColorSetA(0, mask->getAddr1(x, y));
+            return SkColorSetA(0, *mask->getAddr1(x, y));
         case SkMask::Format::kA8_Format:
-            return SkColorSetA(0, mask->getAddr8(x, y));
+            return SkColorSetA(0, *mask->getAddr8(x, y));
         case SkMask::Format::kARGB32_Format:
-            return mask->getAddr32(x, y);
+            return *mask->getAddr32(x, y);
         case SkMask::Format::kLCD16_Format:
-            return SkPixel16ToColor(mask->getAddr16(x, y));
+            return SkPixel16ToColor(*mask->getAddrLCD16(x, y));
         case SkMask::Format::k3D_Format:
             return 0;
     }
